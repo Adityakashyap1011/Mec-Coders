cf5c7ceff689a91ffcddcfc8c748c5f38069ed8f
Swayam Sharma <142594033+23135133@users.noreply.github.com>
2025-02-02 23:20:16 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+import cors from "cors";
 import express from "express";
 import geminiRouter from "./routes/gemini.js"; 
 
 const app = express();
-const port = 8080;
+const port = 3000;
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/api/gemini", geminiRouter);
 
 app.listen(port, () => {
