003da5d537dfbad0828f1f88f5fec35aa7b9d4d8
Aditya <adityakashyap1011@gmail.com>
2025-02-03 08:51:53 +0530

    Update CodeEditor.jsx,added collab feature

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -4,6 +4,11 @@ import { Editor } from "@monaco-editor/react";
 import LanguageSelector from "./LanguageSelector";
 import { CODE_SNIPPETS } from "../constants";
 import Output from "./Output";
+import * as YJS from "yjs"
+import {WebrtcProvider} from "y-webrtc"
+import {MonacoBinding} from "y-monaco"
+
+
 
 const CodeEditor = () => {
   const editorRef = useRef();
@@ -13,6 +18,11 @@ const CodeEditor = () => {
   const onMount = (editor) => {
     editorRef.current = editor;
     editor.focus();
+    const document =new YJS.Doc(); 
+    const RtcProvider =new WebrtcProvider("EDITOR-ROOM", document);
+    const Binding =new MonacoBinding(document.getText("monacoEditor") , editorRef.current.getModel(), new Set([editorRef.current]), RtcProvider.awareness);
+    console.log(RtcProvider.awareness);  
+    
   };
 
   const onSelect = (language) => {
@@ -46,3 +56,5 @@ const CodeEditor = () => {
   );
 };
 export default CodeEditor;
+
+
